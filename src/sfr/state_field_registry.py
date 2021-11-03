9e3a00184b088a4ca68e99ce3255b2f12b63859c
Shihao Cao <shehowcow@gmail.com>
2021-11-03 17:22:08 -0400

    remove unnecessary changes

--- src/sfr/state_field_registry.py
+++ src/sfr/state_field_registry.py
@@ -1,19 +1,16 @@
 class StateFieldRegistry():
-    def __init__(self):
-        self.fields = {}
+  def __init__(self):
+    self.fields = {}
 
-    """ Set a single value in the SFR. """
+  """ Set a single value in the SFR. """
+  def set(self, key: str, value):
+    self.fields[key] = value
 
-    def set(self, key: str, value):
-        self.fields[key] = value
+  """ Returns None if [key] is not in SFR """
+  def get(self, key: str):
+    return self.fields.get(key)
 
-    """ Returns None if [key] is not in SFR """
-
-    def get(self, key: str):
-        return self.fields.get(key)
-
-    """ SFR as a dictionary, with keys and values """
-
-    def as_dict(self):
-        # a deep copy, use copy() if the dict will never contain compound objects
-        return self.fields.deepcopy()
+  """ SFR as a dictionary, with keys and values """
+  def as_dict(self):
+    # a deep copy, use copy() if the dict will never contain compound objects
+    return self.fields.deepcopy()
