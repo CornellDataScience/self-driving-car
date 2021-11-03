34a91650fcbf988dc19363d2c5f8a033b66802e0
Shihao Cao <shehowcow@gmail.com>
2021-11-03 17:09:59 -0400

    add docstrings

--- src/framework/main_control_loop.py
+++ src/framework/main_control_loop.py
@@ -5,8 +5,7 @@ import time
 
 class MainControlLoop(ControlTaskBase):
     def setup(self):
-        # self.sfr = StateFieldRegistry()
-
+        """All the setup required for the MainControlLoop."""
         self.sfr = StateFieldRegistry()
 
         self.clock_manager = ClockManager(self.config, self.sfr)
@@ -18,18 +17,21 @@ class MainControlLoop(ControlTaskBase):
         self.setup_control_tasks()
 
     def default(self) -> None:
+        """Call the default functions of all the ControlTasks."""
         self.clock_manager.default()
         self.read_camera.default()
         self.point_tracker.default()
         self.mission_manager.default()
 
     def setup_control_tasks(self):
+        """Call the setup functions of all the ControlTasks."""
         self.clock_manager.setup()
         self.read_camera.setup()
         self.point_tracker.setup()
         self.mission_manager.setup()
 
     def execute(self):
+        """Call execute on all control tasks in order."""
         self.clock_manager.execute()
         self.mission_manager.execute()
         time.sleep(0.1) #TODO #3, remove this
