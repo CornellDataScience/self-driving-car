b3805fa3152c58d9962d916d5e98a4d06fc39291
Edward Gu <elg227@cornell.edu>
2021-11-03 17:14:58 -0400

    draft

--- src/ControlTasks/read_camera.py
+++ src/ControlTasks/read_camera.py
@@ -7,24 +7,19 @@ class ReadCamera(ControlTaskBase):
 
     def initialize(self):
         vc = cv2.VideoCapture(0)
-
         if vc.isOpened():
             # try to get the first frame
             rval, frame = vc.read()
-            prev_frame = None
-        else:
-            rval = False
+        vc.release()
 
     def default(self):
+        self.sfr.set("frame", 0)
 
     def execute(self):
         print("Reads frame from camera")
         vc = cv2.VideoCapture(0)
         # get next frame from camera and push to statefeldregistry
         while rval:
-            prev_frame = frame
             rval, frame = vc.read()
-
         vc.release()
-
         self.sfr["frame"] = self.frame
