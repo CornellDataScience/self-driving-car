54f628d506c373ed616c255742fb3507bd2c3bfc
Iris <icl5@cornell.edu>
2021-11-10 16:32:51 -0500

    added basic argument parser for config and execution mode. Would a more concise library for parsing be more ideal?

--- src/runner/test_case_system.py
+++ src/runner/test_case_system.py
@@ -1,5 +1,6 @@
 import sys
 import yaml
+import argparse
 from src.framework.main_control_loop import MainControlLoop
 from src.sfr.state_field_registry import StateFieldRegistry
 # from src.runner import * # must import all test instance classes, eg clock_test, no_sim_test
@@ -10,11 +11,18 @@ from .camera_test import CameraTest
 class TestCaseSystem:
     def run(self):
         # creates necessary objects
-        if len(sys.argv) > 2:
-            # Read YAML file
-            file_path = sys.argv[1]
-        else:
-            file_path = 'src/config.yaml'
+        parser = argparse.ArgumentParser("config file, run mode")
+        parser.add_argument("--c", type=str,
+        help="An optional argument. The configuration file")
+        parser.add_argument("--m", type=str,
+        help="An optional argument. Specifies which mode to run in. By default, runs NoSIM")
+
+        args = parser.parse_args()
+
+        # Read YAML file
+        file_path = "src/config.yaml"
+        if args.c != None:
+            file_path = args.c
 
         with open(file_path, 'r') as stream:
             self.config = yaml.safe_load(stream)
@@ -24,7 +32,10 @@ class TestCaseSystem:
         self.mcl.setup()
 
         # load test case
-        test = self.load_test_case("NoSIM")
+        test_case_name = "NoSIM"
+        if args.m != None:
+            test_case_name = args.m
+        test = self.load_test_case(test_case_name)
         test.run()
 
     def load_test_case(self, test_case_name):
