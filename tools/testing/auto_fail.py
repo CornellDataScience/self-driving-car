9dfadb125e714c405b81bacade02249e5223649e
Shihao Cao <shehowcow@gmail.com>
2021-11-13 15:44:39 -0500

    testing hootl cases

---
+++ tools/testing/auto_fail.py
@@ -0,0 +1,2 @@
+'''An intentionally failing script for tesitng the CI pipeline.'''
+raise AssertionError('Intentionally Failing Python Script')
---
+++ tools/testing/auto_pass.py
@@ -0,0 +1,3 @@
+'''A testing case that passes, for testing the CI pipeline.'''
+var = 'Hello World!'
+print(var)
