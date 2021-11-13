f1dfc7a594dabe4d3a5b10ccbc9abb33038c31a5
fort745 <44331314+fort745@users.noreply.github.com>
2021-11-13 17:39:26 -0500

    Argparser (#16)
    * added argument parser to take in config file path and test case name arguments
    
    * fixed imports
    
    * deleted extraneous code
    
    Co-authored-by: Iris <icl5@cornell.edu>

--- src/runner/camera_test.py
+++ src/runner/camera_test.py
@@ -1,8 +1,9 @@
 from src.runner.testing_instance import TInstance
 
+
 class CameraTest(TInstance):
     def cycle(self):
-        self.mcl.execute() # set hardware components to execute decision
+        self.mcl.execute()  # set hardware components to execute decision
 
     # for now, the same as no_sim test, will diverge later
     def run(self):
--- src/runner/test_case_system.py
+++ src/runner/test_case_system.py
@@ -1,20 +1,20 @@
-import sys
 import yaml
+import argparse
 from src.framework.main_control_loop import MainControlLoop
 from src.sfr.state_field_registry import StateFieldRegistry
 # from src.runner import * # must import all test instance classes, eg clock_test, no_sim_test
-from src.runner.no_sim_test import NoSIM
-# from src.runner.clock_test
+from .clock_test import ClockTest
+from .no_sim_test import NoSIM
+from .camera_test import CameraTest
+
 
 class TestCaseSystem:
     def run(self):
         # creates necessary objects
-        if len(sys.argv) > 2:
-            # Read YAML file
-            file_path = sys.argv[1]
-        else:
-            file_path = 'src/config.yaml'
+        args = self.get_arg_parser()
 
+        # Read YAML file
+        file_path = args.config_file_path
         with open(file_path, 'r') as stream:
             self.config = yaml.safe_load(stream)
 
@@ -23,9 +23,23 @@ class TestCaseSystem:
         self.mcl.setup()
 
         # load test case
-        test = self.load_test_case("NoSIM")
+        test_case_name = args.t
+        test = self.load_test_case(test_case_name)
         test.run()
 
     def load_test_case(self, test_case_name):
         test = globals()[test_case_name]
         return test(self.config, self.sfr, self.mcl)
+
+    def get_arg_parser(self):
+        parser = argparse.ArgumentParser("config file, run mode")
+        parser.add_argument(
+            "config_file_path",
+            type=str,
+            help="A required argument. The path to YAML configuration file.")
+        parser.add_argument(
+            "t",
+            type=str,
+            help="An required argument. Specifies which tests to run.")
+
+        return parser.parse_args()
