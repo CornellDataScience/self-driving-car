8ebb0b76ec2b95bb5fd0c94a12df7d93a2758e90
Shihao Cao <shehowcow@gmail.com>
2021-11-13 16:49:19 -0500

    remove print statement from read_camera

--- src/ControlTasks/read_camera.py
+++ src/ControlTasks/read_camera.py
@@ -17,7 +17,6 @@ class ReadCamera(ControlTaskBase):
         self.sfr.set("prev_frame", None)
 
     def execute(self):
-        print("Reads frame from camera")
         current = self.sfr.get('curr_frame')
         self.sfr.set("prev_frame", current)
         if self.vc.isOpened():
