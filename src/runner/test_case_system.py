50b52cebbeb3add267d0a7ec5cb2b5cc7a916671
Iris <icl5@cornell.edu>
2021-11-13 17:16:32 -0500

    made config file path and test name required arguments to run test cases

--- src/runner/test_case_system.py
+++ src/runner/test_case_system.py
@@ -1,4 +1,3 @@
-import sys
 import yaml
 import argparse
 from src.framework.main_control_loop import MainControlLoop
@@ -8,22 +7,14 @@ from .clock_test import ClockTest
 from .no_sim_test import NoSIM
 from .camera_test import CameraTest
 
+
 class TestCaseSystem:
     def run(self):
         # creates necessary objects
-        parser = argparse.ArgumentParser("config file, run mode")
-        parser.add_argument("--c", type=str,
-        help="An optional argument. The configuration file")
-        parser.add_argument("--t", type=str,
-        help="An optional argument. Specifies which tests to run. By default, runs NoSIM")
-
-        args = parser.parse_args()
+        args = self.get_arg_parser()
 
         # Read YAML file
-        file_path = "src/config.yaml"
-        if args.c != None:
-            file_path = args.c
-
+        file_path = args.config_file_path
         with open(file_path, 'r') as stream:
             self.config = yaml.safe_load(stream)
 
@@ -32,12 +23,23 @@ class TestCaseSystem:
         self.mcl.setup()
 
         # load test case
-        test_case_name = "NoSIM"
-        if args.t != None:
-            test_case_name = args.t
+        test_case_name = args.t
         test = self.load_test_case(test_case_name)
         test.run()
 
     def load_test_case(self, test_case_name):
         test = globals()[test_case_name]
         return test(self.config, self.sfr, self.mcl)
+
+    def get_arg_parser(self):
+        parser = argparse.ArgumentParser("config file, run mode")
+        parser.add_argument(
+            "config_file_path",
+            type=str,
+            help="A required argument. The path to YAML configuration file.")
+        parser.add_argument(
+            "t",
+            type=str,
+            help="An required argument. Specifies which tests to run.")
+
+        return parser.parse_args()
