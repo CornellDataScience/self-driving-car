9d6121813b814a83eaf314896349aa8d812c2793
Eric Zhang <ericzhang090@gmail.com>
2021-11-15 14:11:18 -0500

    need review

--- src/ControlTasks/time_manager.py
+++ src/ControlTasks/time_manager.py
@@ -2,7 +2,8 @@ from .control_task_base import ControlTaskBase
 from ..sfr import StateFieldRegistry
 
 import time
-#control tasks init and sfr init 
+# control tasks init and sfr init
+
 
 class TimeManager(ControlTaskBase):
 
@@ -10,21 +11,30 @@ class TimeManager(ControlTaskBase):
         pass
 
     def default(self):
-        self.sfr.set("start_time",time.time())
-        self.sfr.set("target_control_cycle_duration",0.1)
+        self.sfr.set("start_time", time.time())
+        self.sfr.set("target_control_cycle_duration", 1)
 
     def execute(self):
-        
-        #add start time into sfr 
-        #get start time after all control task calls 
+
+        # add start time into sfr
+        # get start time after all control task calls
+        current_time = time.time()
         start_time = self.sfr.get("start_time")
-        self.default()
         runtime = self.sfr.get("target_control_cycle_duration")
-        
-        if start_time < runtime:
-            time.sleep(runtime-start_time)
 
-        if start_time >= runtime:
-            print("BAD! Ran for "+ str(runtime) + " seconds")
-            
+        if current_time - start_time < runtime:
+            print(current_time)
+            print(start_time)
+            print('elapsed time: ' + str(current_time - start_time))
+            print('runtime: ' + str(runtime))
+            print('time to sleep: ' + str(runtime - (current_time - start_time)))
+            time.sleep(runtime - (current_time - start_time))
+            # while current_time - start_time < runtime:
+            #     time.sleep(0.0000001)
+            #     current_time = time.time()
 
+        if current_time - start_time >= runtime:
+            print("BAD! Ran for more than " + str(runtime) + " seconds")
+            print('Ran for ' + str(current_time - start_time))
+
+        self.sfr.set("start_time", time.time())
