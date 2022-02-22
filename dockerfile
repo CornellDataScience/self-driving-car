bbfbf1f370830e4ea775658c7c80231de23612c3
Tobi Alade <aoa34@cornell.edu>
2022-02-22 16:17:52 -0500

    Qt broken

---
+++ dockerfile
@@ -0,0 +1,23 @@
+# syntax=docker/dockerfile:1
+
+FROM ubuntu:latest
+WORKDIR /app
+
+RUN apt-get update && apt-get autoclean
+
+RUN apt-get update && apt-get install -y --no-install-recommends python3-pip
+
+COPY requirements.txt requirements.txt
+RUN pip3 install -r requirements.txt
+
+RUN pip3 install pyqt5
+
+RUN apt-get install ffmpeg libsm6 libxext6  -y
+
+RUN apt-get install -y '^libxcb.*-dev' libx11-xcb-dev libglu1-mesa-dev libxrender-dev libxi-dev libxkbcommon-dev libxkbcommon-x11-dev 
+
+RUN export DISPLAY=:0.0
+
+COPY . .
+
+CMD [ "python3", "-m", "src", "hootl.yaml", "NoSIM"]
--- src/ControlTasks/clock_manager.py
+++ src/ControlTasks/clock_manager.py
@@ -1,17 +1,16 @@
 from .control_task_base import ControlTaskBase
-import time
 
 
 class ClockManager(ControlTaskBase):
     def setup(self):
         pass
-    
+
     def default(self):
-        self.sfr.set('cycle_num', 0)
+        self.sfr.set("cycle_num", 0)
 
     def execute(self):
-        cycle_num = self.sfr.get('cycle_num')
+        cycle_num = self.sfr.get("cycle_num")
         cycle_num += 1
 
-        print(f'[INFO] Cycle_num: {cycle_num}')
-        self.sfr.set('cycle_num', cycle_num)
+        print(f"[INFO] Cycle_num: {cycle_num}")
+        self.sfr.set("cycle_num", cycle_num)
