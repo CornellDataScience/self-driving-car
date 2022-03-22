07f6d2c128a3cf9e09643358feed5718a051cab0
Edward Gu <elg227@cornell.edu>
2022-03-22 18:11:29 -0400

    lane

---
+++ src/ControlTasks/lane_detection.py
@@ -0,0 +1,12 @@
+from .control_task_base import ControlTaskBase
+
+
+class LaneDetection(ControlTaskBase):
+    def setup(self):
+        pass
+
+    def default(self):
+        pass
+
+    def execute(self):
+        print("")
