1828a896164d4f077e2430e298ec48cd7356d539
Tobi Alade <aoa34@cornell.edu>
2022-04-12 14:49:19 -0400

    initial setup

--- src/ControlTasks/lane_detection.py
+++ src/ControlTasks/lane_detection.py
@@ -1,12 +1,22 @@
 from .control_task_base import ControlTaskBase
 
 
+def run_model(frame):
+    pass
+
+
+def calc_angle(processed_frame):
+    pass
+
+
 class LaneDetection(ControlTaskBase):
     def setup(self):
         pass
 
     def default(self):
-        pass
+        self.sfr.set("angle", 0.0)
 
+    # Take in frame and output direction to move and store in sfr?
     def execute(self):
-        print("")
+        curr_frame = self.sfr.get("curr_frame")
+        self.sfr.set("angle", calc_angle(run_model(curr_frame)))
