a5563a56551e526e950bc3d0fb71e8db863617e9
Evan Williams <emw236@cornell.edu>
2022-04-19 20:52:11 -0400

    updated embedded code

--- embedded/src/fsw/targets/car.cpp
+++ embedded/src/fsw/targets/car.cpp
@@ -2,27 +2,34 @@
 #include <Servo.h>
 #include <Math.h>
 
+#define DIR_PIN 35
+#define STEP_PIN 36
+
 const int drive_to_stepper = 5;
-const int step_size = 10;
+const int max_step_size = 10;
 
-int current = 0; //the current stepper motor angle
+int current = 0; // the current stepper motor angle
 
-//drive_to_stepper_angle(drive) is the discrete stepper motor value that corresponds to a drive angle on the steering wheel
-int drive_to_stepper_angle(int drive) {
+// drive_to_stepper_angle(drive) is the discrete stepper motor value that corresponds to a drive angle on the steering wheel
+int drive_to_stepper_angle(int drive)
+{
     return drive * drive_to_stepper;
 }
 
-void turn_motor(int num_steps) {
-    if(num_steps > 0) {
-        for(int i = 0; i < num_steps; i++) {
-            //turn motor one degree right
-        }
+void turn_motor(int num_steps)
+{
+    if (num_steps > 0)
+        digitalWrite(DIR_PIN, HIGH);
+    else
+        digitalWrite(DIR_PIN, LOW);
+    for (int i = 0; i < num_steps; i++)
+    {
+        digitalWrite(STEP_PIN, HIGH);
+        delayMicroseconds(100);
+        digitalWrite(STEP_PIN, LOW);
+        delayMicroseconds(100);
     }
-    else {
-        for(int i = 0; i < abs(num_steps); i++) {
-            //turn motor one degree left
-        }
-    }
+    current += num_steps;
 }
 
 void execute()
@@ -30,30 +37,28 @@ void execute()
     if (Serial.available() > 0)
     {
         delay(10);
-        int target = Serial.readStringUntil('\n').toInt(); //the target driving wheel angle
+        int target = Serial.readStringUntil('\n').toInt(); // the target driving wheel angle
         int differential = drive_to_stepper_angle(target) - current;
-        if(differential > step_size) {
-            turn_motor(step_size);
+        int to_step = 0;
+
+        if (differential >= 0)
+        {
+            to_step = min(differential, max_step_size);
+            turn_motor(to_step);
         }
-        else if(differential < step_size && differential > 0) {
-            turn_motor(differential);
-        }
-        else if(differential == 0) {
-            turn_motor(0);
-        }
-        else { //differential < step_size && differential < 0
-            if(abs(differential) > step_size) {
-                turn_motor(step_size * -1);
-            }
-            else {
-                turn_motor(differential);
-            }
+        else
+        {
+            to_step = max(differential, -1 * max_step_size);
+            turn_motor(to_step);
         }
     }
 }
 
 void setup()
 {
+    pinMode(STEP_PIN, OUTPUT);
+    pinMode(DIR_PIN, OUTPUT);
+    digitalWrite(STEP_PIN, LOW);
     Serial.begin(115200);
 }
 
