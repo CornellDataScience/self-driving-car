8733f76f0ab2eb4c04943d7f49408c70be8749b7
Evan Williams <emw236@cornell.edu>
2022-05-07 18:25:07 -0400

    updates

--- src/ControlTasks/lane_detection.py
+++ src/ControlTasks/lane_detection.py
@@ -26,9 +26,9 @@ def calc_angle(l, r):
     if road_info == "Straight":
         return 0
     elif road_info == "curving to Left":
-        steering_wheel_angle = -1000 * steering_ratio * math.asin(wheel_base / curvature)
+        steering_wheel_angle = -100 * steering_ratio * math.asin(wheel_base / curvature)
     elif road_info == "curving to Right":
-        steering_wheel_angle = 1000 * steering_ratio * math.asin(wheel_base / curvature)
+        steering_wheel_angle = 100 * steering_ratio * math.asin(wheel_base / curvature)
 
     return steering_wheel_angle
 
@@ -44,9 +44,9 @@ class LaneDetection(ControlTaskBase):
     # Take in frame and output direction to move and store in sfr?
     def execute(self):
         curr_frame = self.sfr.get("curr_frame")
-        curr_frame = cv2.imread(
-            "lane/s-curve-road-skyline-drive-tucked-blue-ridge-mountains-shenandoah-national-park-virgina-31148867.jpeg"
-        )
+        # curr_frame = cv2.imread(
+        #     "lane/s-curve-road-skyline-drive-tucked-blue-ridge-mountains-shenandoah-national-park-virgina-31148867.jpeg"
+        # )
         self.left_line, self.right_line = get_lines.pipeline(curr_frame)
         self.sfr.set("steering_angle", calc_angle(self.left_line, self.right_line))
         print("Angle to turn: " + str(self.sfr.get("steering_angle")))
--- src/ControlTasks/mission_manager.py
+++ src/ControlTasks/mission_manager.py
@@ -3,9 +3,9 @@ from .control_task_base import ControlTaskBase
 
 class MissionManager(ControlTaskBase):
     def setup(self):
-        self.var1 = self.config["var1"]
-        self.var2 = self.config["var2"]
-        self.var3 = self.config["var3"]
+        self.var1 = self.config.get("var1")
+        self.var2 = self.config.get("var2")
+        self.var3 = self.config.get("var3")
         print(self.var1)
         print(self.var2)
         print(self.var3)
